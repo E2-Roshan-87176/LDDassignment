ec6dc943e6d789b596dde3df53e792200575f427
avadanR <awadanroshan01@gmail.com>
2024-12-13 21:25:50 +0530

    Delete ASSIGNMENT_1 directory

--- ASSIGNMENT_1/Q1_split02.c
+++
@@ -1,8 +0,0 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
-
-static void __exit desd_exit(void) {
-    printk(KERN_INFO "%s: desd_exit() called.\n", THIS_MODULE->name);
-}
-
-module_exit(desd_exit);
--- ASSIGNMENT_1/Q2_export.h
+++
@@ -1,7 +0,0 @@
-#ifndef __EXPORT_H
-#define __EXPORT_H
-
-extern int exported_var;
-void exported_fn(void);
-
-#endif
